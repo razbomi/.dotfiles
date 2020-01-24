b230bac430bdfcad25315b20a7c67c1bccb8026f
razbomi <1096424+razbomi@users.noreply.github.com>
2020-01-25 04:05:45 +1100

    Will place reusable scripts here

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ download_dotfiles() {
 
 install_dotfiles() {
   # https://www.gnu.org/software/stow/manual/stow.html
-  stow -vv -d "${INSTALL_DIR}" -S files
+  [[ -a "${HOME}/.bin" ]] || mkdir ${HOME}/.bin
+  stow -vv -d "${INSTALL_DIR}" -S scripts -t ${HOME}/.bin
+  stow -v -d "${INSTALL_DIR}" -S files
 }
 
 download_dotfiles
