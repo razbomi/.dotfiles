abcc0c0d8dbb199e34227c4f63fd840543d395e4
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2023-10-10 00:23:54 +1100

    I don't want the BufOnly script for now

--- files/.config/nvim/lua/custom/mappings.lua
+++ files/.config/nvim/lua/custom/mappings.lua
@@ -7,7 +7,6 @@ M.general = {
 		["<leader>s"] = { ":update<CR>", "save" },
 		["<leader>i"] = { "^", "start of line" },
 		["<leader>a"] = { "$", "end of line" },
-		["<leader>o"] = { ":BufOnly<CR>", "close other buffers" },
 		["Q"] = { ":qa<CR>", "quick close" },
 	},
 	i = {
