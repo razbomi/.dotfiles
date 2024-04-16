48cda408f2f463f679295fe18095d83ac48dad03
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2024-04-16 20:50:05 +1000

    History wrapper for ohmzsh now shows the last 30 or something

--- files/.config/nvim/lua/custom/configs/null-ls.lua
+++ files/.config/nvim/lua/custom/configs/null-ls.lua
@@ -5,34 +5,34 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-	formatting.prettier,
-	formatting.stylua,
-	formatting.goimports,
+  formatting.prettier,
+  formatting.stylua,
+  formatting.goimports,
 
-	formatting.shfmt,
-	lint.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+  formatting.shfmt,
+  lint.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 
-	-- formatting.rustfmt,
-	formatting.terraform_fmt.with({
-		extra_filetypes = { "hcl" },
-	}),
-	-- lint.terraform_validate,
-	-- lint.eslint_d,
+  -- formatting.rustfmt,
+  formatting.terraform_fmt.with({
+    extra_filetypes = { "hcl" },
+  }),
+  -- lint.terraform_validate,
+  -- lint.eslint_d,
 }
 
 null_ls.setup({
-	debug = true,
-	sources = sources,
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
-	end,
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
+  end,
 })
