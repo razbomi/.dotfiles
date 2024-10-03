2d2c157810c7e041b7b21c7985e593c746a02d5d
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2024-10-03 21:34:04 +1000

    Updat api for nvui close buffer

--- files/.config/nvim/lua/chadrc.lua
+++ files/.config/nvim/lua/chadrc.lua
@@ -7,41 +7,6 @@ local M = {}
 M.ui = {
 	statusline = {
 		separator_style = "arrow",
-		order = {
-			"mode",
-			"file",
-			"git",
-			"%=",
-			"lsp_msg",
-			"%=",
-			"diagnostics",
-			"lsp",
-			"yaml",
-			"cwd",
-			"cursor",
-		},
-
-		-- Adds yaml schema type, to help with debugging yamlls
-		modules = {
-			yaml = function()
-				if rawget(vim, "lsp") then
-					for _, client in ipairs(vim.lsp.get_active_clients()) do
-						if
-							client.attached_buffers[vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)]
-							and client.name == "yamlls"
-						then
-							local schema = require("yaml-companion").get_buf_schema(0)
-							return "(" .. schema.result[1].name .. ") "
-						end
-					end
-				end
-				-- local schema = require("yaml-companion").get_buf_schema(0)
-				-- if schema.result[1].name ~= "none" then
-				--   return "(" .. schema.result[1].name .. ") "
-				-- end
-				return ""
-			end,
-		},
 	},
 
 	telescope = {
--- files/.config/nvim/lua/mappings.lua
+++ files/.config/nvim/lua/mappings.lua
@@ -7,6 +7,6 @@ map("i", "jj", "<ESC>", { desc = "General Exit insert mode" })
 map({ "i" }, "<C-s>", "<ESC><cmd> w <cr>", { desc = "General Save and Exit insert mode" })
 map("n", ";", ":", { desc = "General Enter command mode" })
 map("n", "<leader>o", function()
-	local tabufline = require("nvchad.tabufline")
-	tabufline.closeOtherBufs()
+	-- https://github.com/NvChad/ui/blob/6f71760950c89c0484b58520211cc530516849fc/doc/nvui.txt#L269
+	require("nvchad.tabufline").closeAllBufs(false) -- excludes current buf
 end, { desc = "General Close other buffers" })
