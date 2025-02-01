72b2526f329918d77293a028c0127acf89978d29
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2025-02-01 17:53:30 +1100

    This is brew for M macs

--- install/brew.sh
+++ install/brew.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-if ! command -v brew 2>&1; then
+if ! command -v /opt/homebrew/bin/brew 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew update
 brew upgrade
-brew bundle install --file="$(git rev-parse --show-toplevel)
+brew bundle install --file="$(git rev-parse --show-toplevel)/files/.homebrew/Brewfile"
