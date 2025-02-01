1a4929e7f3c5f7fbf72cfcdbf51b697962ced52b
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2025-02-01 17:17:25 +1100

    Add the brew cli

--- install/brew.sh
+++ install/brew.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-# command -v brew 2>&1 || /usr/bin/ruby -e \
-#  "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! command -v brew 2>&1; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew update
 brew upgrade
-brew bundle install --file="$(git rev-parse --show-toplevel)/files/.homebrew/Brewfile"
+brew bundle install --file="$(git rev-parse --show-toplevel)
