37e40761361634d7273c51a03cb7e25c6be6681e
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2025-02-01 18:31:50 +1100

    Won't be for intel

--- install/brew.sh
+++ install/brew.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+# M- mac specific
 if ! command -v /opt/homebrew/bin/brew 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
