e81225a1551ba9ab3732c41195c75528d8a384aa
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2025-03-29 00:09:43 +1100

    Bring back kubes icon

--- install/ohmyzsh.sh
+++ install/ohmyzsh.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 ZSH=${ZSH:-"${HOME}/.oh-my-zsh"}
 
 if [ -d "$ZSH" ]; then
-  $ZSH/tools/upgrade.sh
+  "$ZSH/tools/upgrade.sh"
 else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
