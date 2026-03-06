c2057b2200a4ab060692bf55d614f165afeaca74
Mitko Razboynikov <1096424+razbomi@users.noreply.github.com>
2026-03-07 08:58:27 +1100

    Modernize shell config and aliases
    - Move tmux startup to Ghostty config with linked sessions
    - Fix zshrc source order, hardcode brew prefix, add fzf history bindings
    - Replace z plugin with zoxide, add vi/rdl/rsr fzf functions
    - Update terragrunt aliases to tg* prefix for v0.99 CLI
    - Add tmux and pnpm aliases, uv plugin
    - Bump terraform 1.14.6, terragrunt 0.99.4
    - Remove stale commented-out code and dead aliases

--- files/.oh-my-zsh/custom/aliases_files.zsh
+++ files/.oh-my-zsh/custom/aliases_files.zsh
@@ -1,18 +1,15 @@
-# Goto recent dir
-unalias z 2> /dev/null
+eval "$(zoxide init zsh --no-cmd)"
+
 z() {
-  local dir=$(
-    zshz -l  |
-    cut -wf2 |
-    fzf --height 40% \
-        --layout reverse \
-        --info inline \
-        --tac \
-        --no-sort \
-        --select-1 \
-        --query "$(zshz -e $*)"
-  ) && cd "$dir"
+  local result
+  result=$(zoxide query "$@" 2>/dev/null)
+  if [[ -n "$result" ]]; then
+    cd "$result"
+  else
+    __zoxide_zi "$@"
+  fi
 }
+alias zi="__zoxide_zi"
 
 # cd **<TAB>, <^C-T>
 f() { 
@@ -23,13 +20,52 @@ f() {
         --layout reverse \
         --ignore-case \
         --info inline \
-        --tac \
-        --no-sort \
         --query "$*"
   ) && cd "${dir}"
 }
 
-# v **<TAB> 
 alias v="nvim"
-alias vi="nvim"
 alias vim="nvim"
+
+# Delete lines matching pattern across files (fzf to pick files)
+rdl() {
+  local files
+  files=$(rg "$1" -l |
+    fzf --multi \
+        --tmux 80% \
+        --layout reverse \
+        --preview "rg --color=always -n '$1' {}" \
+        --preview-window right,60%,border-left
+  ) || return
+  echo "$files" | xargs -n1 nvim -nesc ":g/$1/d | wq"
+}
+
+# Search and replace across files (fzf to pick files)
+rsr() {
+  [[ $# -ne 2 ]] && echo "usage: rsr <old> <new>" && return 1
+  local files
+  files=$(rg "$1" -l |
+    fzf --multi \
+        --tmux 80% \
+        --layout reverse \
+        --preview "rg --color=always -n '$1' {}" \
+        --preview-window right,60%,border-left
+  ) || return
+  echo "$files" | xargs sed -i '' "s/$1/$2/g"
+  echo "replaced '$1' → '$2' in:"
+  echo "$files"
+}
+
+vi() {
+  local file
+  file=$(
+    fd --type f --hidden --exclude .git --exclude node_modules |
+    fzf --layout reverse \
+        --ignore-case \
+        --tmux 80% \
+        --query "$*" \
+        --preview "bat --color=always --style=numbers --line-range=:500 {}" \
+        --preview-window right,60%,border-left \
+        --bind 'ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up'
+  ) && nvim "$file"
+}
--- files/.zshrc
+++ files/.zshrc
@@ -9,7 +9,6 @@ COMPLETION_WAITING_DOTS="true"
 DISABLE_UNTRACKED_FILES_DIRTY="true"
 EDITOR=nvim
 HIST_STAMPS="yyyy-mm-dd"
-HYPHEN_INSENSITIVE="true"
 ZSH_THEME=""
 
 # Try presto if this becomes too slow
@@ -20,15 +19,17 @@ plugins=(
   git
   kubectl
   mise
-  # tmux
-  z
+  uv
 )
 
 # https://github.com/zsh-users
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-# source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $ZSH/oh-my-zsh.sh
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Use fzf for history search on up/down (use !! for previous command)
+bindkey '^[[A' fzf-history-widget
+bindkey '^[[B' fzf-history-widget
 
 # https://github.com/sindresorhus/pure?tab=readme-ov-file#oh-my-zsh
 # fpath+=("$(brew --prefix)/share/zsh/site-functions")
@@ -51,18 +52,5 @@ export FZF_DEFAULT_OPTS=" \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
 --multi"
-
-# https://kubernetes.io/docs/reference/kubectl/cheatsheet/#zsh
-# [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
-
-# export PATH="/usr/local/bin:$PATH"
-# export PATH="/usr/local/sbin:$PATH"
-
-if [[ "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]]; then
-  if [[ -z "$TMUX" ]]; then
-    tmux new-session -A -s "$USER"
-  fi
-fi
-
 
 . "$HOME/.local/bin/env"
